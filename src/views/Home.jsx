23b38b4ed4d1cbe4f17863ff631c91a32329bce5
Annemarie <a.lucernoni0@gmail.com>
2023-04-11 12:43:21 -0700

    added create new list button with event handler and passed down token state variable for redirect

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,12 +1,20 @@
 import './Home.css';
+import { useNavigate } from 'react-router-dom';
 
-export function Home() {
+export function Home({ handleClick, token }) {
+	const navigate = useNavigate();
+
+	if (token) {
+		navigate('/list');
+	}
+
 	return (
 		<div className="Home">
 			<p>
 				Hello from the home (<code>/</code>) page!
 			</p>
 			{/* here is a create a new list button with a handleClick action*/}
+			<button onClick={handleClick}>Create a new list!</button>
 		</div>
 	);
 }
