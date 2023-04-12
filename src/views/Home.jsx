c484baffc16db1b9b57845ef514d64d83c6afe7d
Yire Morlans <yire.m@icloud.com>
2023-04-11 20:10:29 -0400

    chore: remove unused imports

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,22 +1,11 @@
 import './Home.css';
-import { useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
 import { generateToken } from '@the-collab-lab/shopping-list-utils';
 
 export function Home({ makeNewList }) {
-	const navigate = useNavigate();
-
 	const handleClick = () => {
 		const newToken = generateToken();
 		makeNewList(newToken);
-		navigate('/list');
 	};
-
-	// useEffect(() => {
-	// 	if (token) {
-	// 		navigate('/list');
-	// 	}
-	// },[navigate, token]);
 
 	return (
 		<div className="Home">
