39e4a2e8c14196ad0091186466ec65807856eee5
Annemarie <a.lucernoni0@gmail.com>
2023-04-18 09:51:00 -0700

    added searchbar with label component and pseudocode

--- src/views/List.jsx
+++ src/views/List.jsx
@@ -6,6 +6,16 @@ export function List({ data }) {
 			<p>
 				Hello from the <code>/list</code> page!
 			</p>
+			{/* add a form component with search bar */}
+			{/* includes text field with semantic "label", working properly if focus changes on clicking*/}
+			{/* filter should match any part of item name not just from start of string */}
+			{/* should include clear field button on search bar */}
+			<form>
+				<label for="searchbar">
+					Filter Items
+					<input type="text" id="searchbar"></input>
+				</label>
+			</form>
 			<ul>
 				{data.map((item) => {
 					return <ListItem key={item.id} name={item.name} />;
