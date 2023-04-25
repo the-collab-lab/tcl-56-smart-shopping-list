08cb74b131f20a78b6250447a292fc0c106dafb6
Yire Morlans <yire.m@icloud.com>
2023-04-25 11:12:48 -0400

    feat: checkItem function calls API to update item properties

--- src/components/ListItem.jsx
+++ src/components/ListItem.jsx
@@ -1,10 +1,16 @@
 import './ListItem.css';
+import { updateItem } from '../api/firebase';
 
-export function ListItem({ name, key }) {
+export function ListItem({ name, itemId, listId }) {
+	const checkItem = (e) => {
+		console.log(e.target, listId, itemId);
+		updateItem(listId, itemId);
+	};
+
 	return (
-		<li class="ListItem">
-			<label htmlFor={key}>
-				<input type="checkbox" id={key} />
+		<li className="ListItem">
+			<label htmlFor={itemId}>
+				<input type="checkbox" id={itemId} onChange={checkItem} />
 				{name}
 			</label>
 		</li>
