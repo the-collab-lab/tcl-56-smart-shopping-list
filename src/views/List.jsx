c4cca918b1bf553ad09d1a56b282215db37ec519
Yire Morlans <yire.m@icloud.com>
2023-05-17 16:01:14 -0400

    feat: add func to allow user to check out of current list back to home page, improve app behavior to prevent user w/o list token to navigate to List

--- src/views/List.jsx
+++ src/views/List.jsx
@@ -1,6 +1,7 @@
 import { ListItem } from '../components';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
+
 import { comparePurchaseUrgency } from '../utils/items';
 import Chippy3 from '/img/Chippy3.gif';
 import './List.css';
@@ -8,26 +9,47 @@ import './List.css';
 export function List({ data, listId }) {
 	// creates a state variable to track searchbar input
 	const [query, setQuery] = useState('');
+	const [listName, setListName] = useState('');
 
 	// sets the state to the searchbar input as the user types
 	const handleSearch = (e) => {
 		setQuery(e.target.value);
 	};
-
 	// clears the searchbar and resets the filtered list to the whole list
 	const clearFilter = (e) => {
 		setQuery('');
 	};
-
 	const navigate = useNavigate();
+
+	const removeListFromStorage = () => {
+		//remove list from local storage
+		localStorage.removeItem('tcl-shopping-list-token');
+		// refresh page to force redirect to home page
+		window.location.reload();
+	};
+
+	useEffect(() => {
+		if (!listId) {
+			navigate('/');
+		}
+		const listName = localStorage.getItem('tcl-shopping-list-token');
+		setListName(listName);
+	}, [listName, listId, navigate]);
 
 	return (
 		<>
-			<p>Welcome to your shopping list!</p>
 			<p>
-				{data.length < 2
-					? 'Your list is empty.  Add an item to get started.'
-					: null}
+				Welcome to your <strong>{listName}</strong> shopping list!
+			</p>
+			<p>
+				{data.length < 2 ? (
+					'Your list is empty.  Add an item to get started.'
+				) : (
+					<>
+						<span>Want to check out a different list? </span>
+						<button onClick={removeListFromStorage}>Click here</button>
+					</>
+				)}
 			</p>
 			<button
 				onClick={() => {
@@ -62,8 +84,8 @@ export function List({ data, listId }) {
 			</ul>
 			<div className="chippy-suggestion chippy-suggestion-bottom-right">
 				{data.length < 2
-					? 'Uh oh! Your shopping list is empty! Try using the "Add Item" button to begin your list!'
-					: 'Yummy! That list is looking good! Did you know that you can use the filter to search within your list?'}{' '}
+					? `Uh oh! ${listName} list is empty! Try using the "Add Item" button to begin your list!`
+					: `Yummy! Your shopping list is looking good! Did you know that you can use the filter to search within ${listName}?`}
 			</div>
 			<img
 				id="chippy"
