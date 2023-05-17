8cecf5442562320ba84eeed36764329384370ab5
Yire Morlans <yire.m@icloud.com>
2023-05-17 16:03:01 -0400

    refactor: edit func to allow input from user instead of generated token

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,12 +1,14 @@
 import { useState } from 'react';
-import { generateToken } from '@the-collab-lab/shopping-list-utils';
+// import { generateToken } from '@the-collab-lab/shopping-list-utils';
 import './Home.css';
 
 export function Home({ makeNewList, joinList, handleError, joinListErrorMsg }) {
 	const [inputValue, setInputValue] = useState('');
+	const [listName, setListName] = useState('');
 
-	const handleClick = () => {
-		const newToken = generateToken();
+	const handleClick = (event) => {
+		event.preventDefault();
+		const newToken = listName.trim().toLowerCase();
 		makeNewList(newToken);
 	};
 	/**
@@ -20,31 +22,41 @@ export function Home({ makeNewList, joinList, handleError, joinListErrorMsg }) {
 
 	return (
 		<div className="Home">
-			<p>
-				Hello from the home (<code>/</code>) page!
-			</p>
+			<p>To create a new shopping list, give your list a name.</p>
 			{handleError ? <span>{handleError}</span> : null}
-			<button onClick={handleClick}>Create a new list!</button>
+			<form onSubmit={handleClick}>
+				<label htmlFor="listName">List name </label>
+				<input
+					type="text"
+					name="listName"
+					id="listName"
+					required
+					value={listName}
+					onChange={(e) => setListName(e.target.value)}
+					pattern="[a-zA-Z0-9\s]+"
+				/>
+				<button type="submit">Create a new list!</button>
+			</form>
 
 			<div className="JoinListForm">
-				<p>Join an existing shopping list by entering a three word token.</p>
+				<p>Join an existing shopping list by entering a list name.</p>
 				{joinListErrorMsg ? <span>{joinListErrorMsg}</span> : null}
 				<form onSubmit={handleJoinList}>
-					<label htmlFor="input">Share token</label>
+					<label htmlFor="input">Shared list name </label>
 					<input
 						name="input"
 						id="input"
 						type="text"
 						value={inputValue}
 						onChange={(event) => setInputValue(event.target.value)}
-						pattern="[a-zA-Z]+ [a-zA-Z]+ [a-zA-Z]+"
-						onInvalid={(e) =>
-							e.target.setCustomValidity(
-								'Please enter 3 words with single space only.',
-							)
-						}
-						onInput={(e) => e.target.setCustomValidity('')}
+						pattern="[a-zA-Z0-9\s]+"
 						required
+						// onInvalid={(e) =>
+						// 	e.target.setCustomValidity(
+						// 		'Please enter a valid list name i.e no numbers.',
+						// 	)
+						// }
+						// onInput={(e) => e.target.setCustomValidity('')}
 					/>
 					<button type="submit">Join an existing list</button>
 				</form>
