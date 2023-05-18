8e3968ac406d3c56f0c9f901cf5cc2911f9de22c
Drake Nguyen <drakeknguyen@yahoo.com>
2023-05-17 19:55:26 -0700

    grouped features

--- src/views/List.jsx
+++ src/views/List.jsx
@@ -23,53 +23,60 @@ export function List({ data, listId }) {
 
 	return (
 		<>
-			<p>Welcome to your shopping list!</p>
-			<p>
-				{data.length < 2
-					? 'Your list is empty.  Add an item to get started.'
-					: null}
-			</p>
-			<button
-				onClick={() => {
-					navigate('/add-item');
-				}}
-			>
-				Add Item
-			</button>
-			<form>
-				<label htmlFor="searchbar">
-					Filter Items
-					<input
-						type="text"
-						id="searchbar"
-						onChange={handleSearch}
-						value={query}
-					></input>
-				</label>
-				<button type="button" onClick={clearFilter}>
-					Clear
-				</button>
-			</form>
-			<ul>
-				{/* filters the list to match the user's query as user types, then maps over the items that match the filter to display them */}
-				{comparePurchaseUrgency(data)
-					.filter((item) =>
-						item.name?.toLowerCase().includes(query.toLowerCase()),
-					)
-					.map((item) => {
-						return <ListItem key={item.id} item={item} listId={listId} />;
-					})}
-			</ul>
-			<div className="chippy-suggestion chippy-suggestion-bottom-right">
-				{data.length < 2
-					? 'Uh oh! Your shopping list is empty! Try using the "Add Item" button to begin your list!'
-					: 'Yummy! That list is looking good! Did you know that you can use the filter to search within your list?'}{' '}
+			{/* <p>Welcome to your shopping list!</p> */}
+			<div id="chippyBox">
+				<div className="chippy-suggestion chippy-suggestion-bottom-right">
+					{data.length < 2
+						? 'Uh oh! Your shopping list is empty! Try using the "Add Item" button to begin your list!'
+						: 'Yummy! That list is looking good! Did you know that you can use the filter to search within your list?'}{' '}
+				</div>
+				<img
+					id="chippy"
+					src={Chippy3}
+					alt='Helpful potato chip gif named Chippy, à la "Clippy", who suggests that user adds an item to their shopping list '
+				></img>
 			</div>
-			<img
-				id="chippy"
-				src={Chippy3}
-				alt='Helpful potato chip gif named Chippy, à la "Clippy", who suggests that user adds an item to their shopping list '
-			></img>
+			<div id="searchList">
+				<p>
+					{data.length < 2
+						? 'Your list is empty.  Add an item to get started.'
+						: null}
+				</p>
+				<button
+					onClick={() => {
+						navigate('/add-item');
+					}}
+				>
+					Add Item
+				</button>
+				<form>
+					<label htmlFor="searchbar">
+						Filter Items
+						<input
+							type="text"
+							id="searchbar"
+							onChange={handleSearch}
+							value={query}
+						></input>
+					</label>
+					<button type="button" onClick={clearFilter}>
+						Clear
+					</button>
+				</form>
+			</div>
+			<div id="itemList">
+				<ul>
+					{/* filters the list to match the user's query as user types, then maps over the items that match the filter to display them */}
+					{comparePurchaseUrgency(data)
+						.filter((item) =>
+							item.name?.toLowerCase().includes(query.toLowerCase()),
+						)
+						.map((item) => {
+							return <ListItem key={item.id} item={item} listId={listId} />;
+						})}
+				</ul>
+			</div>
+			<div id="legend"></div>
 		</>
 	);
 }
