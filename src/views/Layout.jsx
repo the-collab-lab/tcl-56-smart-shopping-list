254902604982b1e1bd2791c195e0926f030d8425
Drake Nguyen <drakeknguyen@yahoo.com>
2023-05-22 18:29:04 -0700

    update font style

--- src/views/Layout.jsx
+++ src/views/Layout.jsx
@@ -21,13 +21,13 @@ export function Layout() {
 					<Outlet />
 				</main>
 				<nav className="Nav">
-					<NavLink to="/" className="Nav-link">
+					<NavLink to="/" className="Nav-link underline">
 						Home
 					</NavLink>
-					<NavLink to="/list" className="Nav-link">
+					<NavLink to="/list" className="Nav-link underline">
 						List
 					</NavLink>
-					<NavLink to="/add-item" className="Nav-link">
+					<NavLink to="/add-item" className="Nav-link underline">
 						Add Item
 					</NavLink>
 				</nav>
