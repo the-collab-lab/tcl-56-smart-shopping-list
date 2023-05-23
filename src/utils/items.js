837ca6ceccf75770a520c8d5939ec66731189e11
Yire Morlans <yire.m@icloud.com>
2023-05-23 14:14:29 -0400

    refactor: delete extra date calculating functions and utilized getDaysBetweenDates for comparing purchase urgency

--- src/utils/items.js
+++ src/utils/items.js
@@ -1,21 +1,5 @@
 import { transformToJSDate, getDaysBetweenDates } from './dates';
 
-export function getItemDaysUntilNextPurchase(item) {
-	const today = new Date();
-	const nextPurchase = transformToJSDate(item.dateNextPurchased);
-
-	return getDaysBetweenDates(nextPurchase, today);
-}
-
-export function getItemDaysSinceLastPurchase(item) {
-	const today = new Date();
-	const lastPurchase = transformToJSDate(
-		item.dateLastPurchased || item.dateCreated,
-	);
-
-	return getDaysBetweenDates(lastPurchase, today);
-}
-
 export function sortItems(data) {
 	return data.sort((a, b) => {
 		const currentDate = transformToJSDate(a.dateNextPurchased);
@@ -34,13 +18,23 @@ export function sortItems(data) {
 export function comparePurchaseUrgency(data) {
 	const inactiveItems = sortItems(
 		data.filter((item) => {
-			return getItemDaysSinceLastPurchase(item) >= 60;
+			return (
+				getDaysBetweenDates(
+					transformToJSDate(item.dateLastPurchased || item.dateCreated),
+					new Date(),
+				) >= 60
+			);
 		}),
 	);
 
 	const activeItems = sortItems(
 		data.filter((item) => {
-			return getItemDaysSinceLastPurchase(item) < 60;
+			return (
+				getDaysBetweenDates(
+					transformToJSDate(item.dateLastPurchased || item.dateCreated),
+					new Date(),
+				) < 60
+			);
 		}),
 	);
 
